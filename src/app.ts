fd4d424692661cbcc762badbd5a9b881be10f0fb
clement_obert <CObert.prestataire@oui.sncf>
2020-08-19 11:37:12 +0200

    init socket.io + add basic navigation

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import http from 'http';
 import bodyParser from 'body-parser';
 import config from 'config';
 import axios from 'axios';
+import socketIo from 'socket.io';
 
 process.title = config.get("process.title");
 
@@ -14,22 +15,35 @@ const API_HOST: string = config.get("api.host");
 
 app.set('views', __dirname + '/../views');
 // static resources
-app.use('/static', express.static(__dirname + '/../client'));
+app.use('/public', express.static(__dirname + '/../client'));
 // support json encoded bodies
 app.use(bodyParser.json());
 // support encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// redirect some URLs to tactic API
-app.get('/api/**', function (req, res) {
-	const url: string = API_HOST + req.path.split("/api")[1];
+app.get('/', function (req, res) {
+	res.render("index.ejs");
+});
+
+app.get('/games', function (req, res) {
+	// Load list of games
+	const url: string = API_HOST + "/api/games/";
 	axios.get(url)
-		.then((resp) => res.json(resp.data))
-		.catch((error) => {
-			res.render("error.ejs", { error: error.response || error });
-		});
+		.then((resp) => res.render("games.ejs", {games: resp.data}))
+		.catch((error) => res.render("error.ejs", {error: error}));
 });
 
 server.listen(EXPRESS_PORT_NUMBER, function () {
 	console.log('TRPG listening on port ' + EXPRESS_PORT_NUMBER);
+});
+
+// Chargement de socket.io
+const io = socketIo.listen(server);
+
+io.on('connect', (socket) => {
+	console.log(socket.id + " : User connected");
+
+	socket.on('disconnect', (reason: string) => {
+		console.log(socket.id + " : User disconnected");
+	});
 });
